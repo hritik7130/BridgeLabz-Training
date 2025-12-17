2a791ab8422efb6267534d12a3134c819cfc19bc
hritik7130 <hritik.chauhan_cs22@gla.ac.in>
2025-12-17 15:36:27 +0530

    Welcome to BridgeLabz

--- leet-code-codebase/factors.java
+++ leet-code-codebase/factors.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Factors {
+public class factors {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int input=sc.nextInt();
