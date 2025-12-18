1dd2c3ce00907d03765da503c543e0603b94d70c
hritik7130 <hritik.chauhan_cs22@gla.ac.in>
2025-12-18 16:11:17 +0530

    Add program of handhske problem

---
+++ gcr-codebase/programming-elements/Handshake.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Handshake {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numberOfStudents;
+        System.out.print("Enter number of students: ");
+        numberOfStudents = sc.nextInt();
+
+        int handshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+
+        System.out.println("Maximum number of handshakes is " + handshakes);
+        sc.close();
+        
+    }
+}
