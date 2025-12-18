7eac2c212879cabf8c7c3d7b150035d8449ace07
hritik7130 <hritik.chauhan_cs22@gla.ac.in>
2025-12-18 11:00:11 +0530

    Program to find average

---
+++ gcr-codebase/programming-elements/AverageMarks.java
@@ -0,0 +1,11 @@
+public class AverageMarks {
+   public static void main(String[] args) {
+    int Maths=94;
+    int Physics=95;
+    int Chemistry=96;
+    int total_marks=Maths+Physics+Chemistry;
+    double average=(total_marks/3)*100;
+    System.out.println(average);
+   }
+    
+}
