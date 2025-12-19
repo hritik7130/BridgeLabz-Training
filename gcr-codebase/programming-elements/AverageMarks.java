4877dcc004341bb5d3cc8881008d21ef653e71be
hritik7130 <hritik.chauhan_cs22@gla.ac.in>
2025-12-19 11:19:11 +0530

    Correct the naming convention

--- gcr-codebase/programming-elements/AverageMarks.java
+++ gcr-codebase/programming-elements/AverageMarks.java
@@ -1,9 +1,9 @@
 public class AverageMarks {
    public static void main(String[] args) {
-    int Maths=94;
-    int Physics=95;
-    int Chemistry=96;
-    int total_marks=Maths+Physics+Chemistry;
+    int maths=94;
+    int physics=95;
+    int chemistry=96;
+    int total_marks=maths+physics+chemistry;
     double average=(total_marks/3)*100;
     System.out.println(average);
    }
