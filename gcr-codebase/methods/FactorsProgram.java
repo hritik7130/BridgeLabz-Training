0597b2f64f83550101156ebe512e5afa5f73ffc5
hritik7130 <hritik.chauhan_cs22@gla.ac.in>
2025-12-23 15:50:29 +0530

    [Hritik] Add . Program to find sum of natural number using methods

---
+++ gcr-codebase/methods/FactorsProgram.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class FactorsProgram {
+
+   
+    static int[] findFactors(int number) {
+        int count = 0;
+
+   
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+
+       
+        int[] factors = new int[count];
+        int index = 0;
+
+      
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                factors[index++] = i;
+            }
+        }
+
+        return factors;
+    }
+
+   
+    static int findSum(int[] factors) {
+        int sum = 0;
+        for (int f : factors) {
+            sum += f;
+        }
+        return sum;
+    }
+
+   
+    static long findProduct(int[] factors) {
+        long product = 1;
+        for (int f : factors) {
+            product *= f;
+        }
+        return product;
+    }
+
+    // Method to find sum of squares of factors
+    static double findSumOfSquares(int[] factors) {
+        double sum = 0;
+        for (int f : factors) {
+            sum += Math.pow(f, 2);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int[] factors = findFactors(number);
+
+        System.out.println("Factors of " + number + ":");
+        for (int f : factors) {
+            System.out.print(f + " ");
+        }
+
+        System.out.println("\nSum of factors: " + findSum(factors));
+        System.out.println("Product of factors: " + findProduct(factors));
+        System.out.println("Sum of squares of factors: " + findSumOfSquares(factors));
+    }
+}
