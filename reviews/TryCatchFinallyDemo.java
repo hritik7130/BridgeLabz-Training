c21d9a8816823090ec49aaff6efb8471ca019a02
hritik7130 <hritik.chauhan_cs22@gla.ac.in>
2026-01-06 00:11:17 +0530

    [Hritik] Add . Review programs

---
+++ reviews/TryCatchFinallyDemo.java
@@ -0,0 +1,19 @@
+public class TryCatchFinallyDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            int a = 10;
+            int b = 2;
+
+            int result = a / b;
+            System.out.println("Result: " + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Error occurred");
+
+        } finally {
+            System.out.println("Program finished");
+        }
+    }
+}
