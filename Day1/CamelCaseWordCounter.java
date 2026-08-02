9b414dda0159ebdf5d424ec8e44eb6190c8cd017
hritik7130 <hritik.chauhan_cs22@gla.ac.in>
2026-08-02 21:13:16 +0530

    Add CamelCaseWordCounter class to count the number of words in a camel case string

---
+++ Day1/CamelCaseWordCounter.java
@@ -0,0 +1,24 @@
+package Day1;
+    import java.util.Scanner;
+
+public class CamelCaseWordCounter {
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        int count = 1; 
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+     sc.close();
+    
+}
+}
