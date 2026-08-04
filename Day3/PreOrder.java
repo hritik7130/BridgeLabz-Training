2eff149eee0417d45d150fd895abf1ae07995076
hritik7130 <hritik.chauhan_cs22@gla.ac.in>
2026-08-04 13:27:29 +0530

    [Hritik] Add PreOrder class to implement pre-order traversal of a binary tree

---
+++ Day3/PreOrder.java
@@ -0,0 +1,38 @@
+package Day3;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class PreOrder {
+
+   
+    static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+    
+
+        Node root = new Node(1);
+        root.right = new Node(2);
+        root.right.right = new Node(5);
+        root.right.right.left = new Node(3);
+        root.right.right.left.right = new Node(4);
+        root.right.right.right = new Node(6);
+
+        preOrder(root);
+    }
+}
