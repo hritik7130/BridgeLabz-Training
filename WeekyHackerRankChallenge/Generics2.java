746d8bd5cde100c8f3e2af81fbff075ba550839d
hritik7130 <hritik.chauhan_cs22@gla.ac.in>
2026-08-21 09:57:42 +0530

    [Hritik] Add Generics2 class with printArray method for generic array output

---
+++ WeekyHackerRankChallenge/Generics2.java
@@ -0,0 +1,20 @@
+package WeekyHackerRankChallenge;
+
+import java.io.*;
+import java.util.*;
+
+public class Generics2 {
+
+ public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);}
+        }
+    public static void main(String[] args) {
+         Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+
+        printArray(intArray);
+        printArray(stringArray);
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
